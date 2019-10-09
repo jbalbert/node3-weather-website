04bdc5ef40b4454dd0b2f4f59c3203f0f950ecfb
JB Albert Joligon <jbalbertjoligon@gmail.com>
2019-10-09 11:54:39 +0800

    add new notes on node course

--- LessonNote.js
+++ LessonNote.js
@@ -27,7 +27,42 @@
             * create .gitignore file 
             * store folders or files you don't want to add to your git repository
         + git add . (to add everyhing in the staging area)
+        + git status
         + git commit -m "Initial Commit"
+    10) Setting up SSH Keys on Github and Heroku
+        + SSH means Secure Shell
+        + Terminal Command
+            * Run Command to see if ssh is already added
+            1) "ls -a -l ~/.ssh" (This is view all files [ as well as hidden files ])
+            * if you have a file ( id_rsa and id_rsa.pub ) that means you already have 
+            * an ssh keys and you could chose those instead of creating a new one.
+            2) next command creating ssh-keygen, ssh-keygen -t rsa -b 4096 -C "jbalbertjoligon@gmail.com"
+            * -b is bits, 4096 is the standard | -C is comment 
+            * just hit enter to create the ssh keys
+            * then run "ls -a -l ~/.ssh"
+            * id_rsa - is a secret file that we need to keep in our machine
+            * id_rsa.pub - public file this is what we are going to share to other 3rd party app.
+            * next command, is make sure the program is running
+            3) eval "$(ssh-agent -s)" this is to start up ssh agent
+            * last is to register the file
+            4) ssh-add ~/.ssh/id_rsa
+    11) Pushing Code to Github After setting up ssh keys
+            * git remote add origin https://github.com/jbalbert/node3-weather-website.git
+            * git remote -v
+            * git push -u origin master -> will not work if you did not configure the ssh keys in your
+            * github account. To do that follow steps below :
+                + Git Account -> Go to Settings ->
+                + SSH + GPG Keys -> New SSH Key
+                + to get the ssh key
+                + run command on terminal
+                    * cat ~/.ssh/id_rsa.pub
+                + copy everyting in the output
+                + then paste to github
+                + to test github connection
+                + run command
+                    * ssh -T git@github.com
+        
+    
 
 
 
